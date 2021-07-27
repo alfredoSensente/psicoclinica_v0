febaeafc4c18a5484ce01c94c6316cc0f75ae6ac
Alfredo J. Sensente <ajsnsn93@gmail.com>
2021-07-26 19:49:20 -0600

    se creo una nueva view que consulta a todos los pacientes registrados en paciente.views

--- clinica/paciente/views.py
+++ clinica/paciente/views.py
@@ -1,3 +1,16 @@
+#Django
 from django.shortcuts import render
 
+#Models
+from .models import Paciente
+
 # Create your views here.
+def index(request):
+    """
+    Devuelve un listado de pacientes
+    """
+    lista_paciente = Paciente.objects.all()
+    context = {
+        'lista_paciente' : lista_paciente,
+    }
+    return render(request, 'paciente/index.html', context)

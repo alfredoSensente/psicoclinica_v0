c61aa7a03df31124e4d592dd4b133a2b8ae885f2
Alfredo J. Sensente <ajsnsn93@gmail.com>
2021-08-06 21:40:20 -0600

    app "paciente" already can add new patients

--- clinica/paciente/forms.py
+++ clinica/paciente/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.db.models.fields import DateField, TextField
+from django.forms import widgets
+from django.forms.widgets import Textarea, Widget
 from .models import Paciente
 
 class PacienteForm(forms.ModelForm):
@@ -16,3 +19,11 @@ class PacienteForm(forms.ModelForm):
             'id_estado_civil',
             'id_sexo',
         )
+        widgets = {
+            'nombre':forms.TextInput(attrs={'type':'text', 'class':'form-control'}),
+            'apellido':forms.TextInput(attrs={'type':'text', 'class':'form-control'}),
+            'fecha_nacimiento' : forms.TextInput(attrs={'type':'date', 'class':'form-control'}),
+            'id_religion':forms.Select(attrs={'type':'text', 'class':'form-select'}),
+            'id_estado_civil':forms.Select(attrs={'type':'text', 'class':'form-select'}),
+            'id_sexo':forms.Select(attrs={'type':'text', 'class':'form-select'}),
+        }

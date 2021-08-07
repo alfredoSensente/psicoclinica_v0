dad5f5f48fdc64920a10173b9900d70c933cd1de
Alfredo J. Sensente <ajsnsn93@gmail.com>
2021-08-07 13:09:11 -0600

    "Patient" app can display patient details.

--- clinica/paciente/views.py
+++ clinica/paciente/views.py
@@ -1,25 +1,28 @@
-#Django
+# Django
 from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import redirect, render, get_object_or_404
 from django.urls import reverse_lazy
 
-#Forms
+# Forms
 from .forms import PacienteForm
 
-#Models
+# Models
 from .models import Paciente
 
 # Create your views here.
+
+
 def index(request):
     """
     Devuelve un listado de pacientes
     """
     lista_paciente = Paciente.objects.all()
     context = {
-        'lista_paciente' : lista_paciente,
-        'active_paciente' : 'active',
+        'lista_paciente': lista_paciente,
+        'active_paciente': 'active',
     }
     return render(request, 'paciente/index.html', context)
+
 
 def nuevo_paciente(request):
     if request.method == 'POST':
@@ -30,36 +33,48 @@ def nuevo_paciente(request):
     else:
         return render(request, 'paciente/nuevo.html', context={'form': PacienteForm()})
 
-def editar_paciente(request,pk):
+
+def editar_paciente(request, pk):
     """
     update a patient
     """
-    paciente_editar = get_object_or_404(Paciente, pk = pk)
+    paciente_editar = get_object_or_404(Paciente, pk=pk)
     if request.method == 'POST':
-        form = PacienteForm(request.POST, instance = paciente_editar)
+        form = PacienteForm(request.POST, instance=paciente_editar)
         if form.is_valid():
             form.save()
             return redirect('paciente:index')
     else:
-        form = PacienteForm(instance = paciente_editar)
-    context={
-        'form' : form,
-        'pk' : pk,
+        form = PacienteForm(instance=paciente_editar)
+    context = {
+        'form': form,
+        'pk': pk,
     }
     return render(request, 'paciente/editar.html', context)
-    
-        
+
+
+def detalle_paciente(request, pk):
+    """
+    Patient detail
+    """
+    paciente_detalle = get_object_or_404(Paciente, pk=pk)
+    context = {
+        'paciente': paciente_detalle
+    }
+    return render(request, 'paciente/detalle.html', context)
+
+
 def borrar_paciente(request, pk):
     """
     Borrar a un paciente a traves de su pk
     """
-    paciente_borrar = get_object_or_404(Paciente, pk = pk)
+    paciente_borrar = get_object_or_404(Paciente, pk=pk)
 
-    if request.method == 'POST':    
+    if request.method == 'POST':
         paciente_borrar.delete()
         return render(request, 'paciente/borrar.html')
     else:
         context = {
-            'paciente_borrar' : paciente_borrar
+            'paciente_borrar': paciente_borrar
         }
         return render(request, 'paciente/borrar.html', context)
